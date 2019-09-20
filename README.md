9f5859117d39549537003a4b48efcf5a6c1e45da
henderjon <list@box370.net>
2019-09-20 07:51:12 -0500

    adds compiler version

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ BIN=example
 HEAD=$(shell git describe --dirty --long --tags 2> /dev/null  || git rev-parse --short HEAD)
 TIMESTAMP=$(shell date '+%Y-%m-%dT%H:%M:%S %z %Z')
 
-LDFLAGS="-X main.buildVersion=$(HEAD) -X 'main.buildTimestamp=$(TIMESTAMP)'"
+LDFLAGS="-X main.buildVersion=$(HEAD) -X 'main.buildTimestamp=$(TIMESTAMP)' -X 'main.compiledBy=$(shell go version)'"
 
 all: darwin64
 
@@ -23,6 +23,6 @@ clean:
 	rm -f $(BIN) $(BIN)-*
 
 .PHONY: darwin64
-darwin64:
+darwin64: clean
 	env GOOS=darwin GOARCH=amd64 go build -ldflags $(LDFLAGS) -o $(BIN)-darwin64-$(HEAD)
 ```
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ var (
 	buildVersion string
 	// BuildTimestamp adds build information
 	buildTimestamp string
+	// CompiledBy adds the make/model that was used to compile
+	compiledBy string
 )
 
 // GetBuildVersion returns the build version set when the binary was built
@@ -16,4 +18,9 @@ func getBuildVersion() string {
 // GetBuildTimestamp returns the build timestamp set when the binary was built
 func getBuildTimestamp() string {
 	return buildTimestamp
+}
+
+// GetBuildTimestamp returns the build timestamp set when the binary was built
+func getCompiledBy() string {
+	return compiledBy
 }
